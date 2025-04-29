f2abba31d91a1025b60419b3d5c00b8ed9c03730
McGaelen <mcinteeg@mail.gvsu.edu>
2025-04-28 21:35:21 -0400

    fix hover box-shadow from obsidian, add lined background

--- src/react/Background.tsx
+++ src/react/Background.tsx
@@ -1,3 +1,15 @@
+import { LinedBackground } from './backgrounds/LinedBackground'
+
 export function Background() {
-  return <div style={{width: '100%', height: '100%', backgroundColor: 'var(--background-primary)' }}></div>
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'var(--background-primary)'
+      }}
+    >
+      <LinedBackground />
+    </div>
+  )
 }
