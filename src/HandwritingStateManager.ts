50a45d5b0350db92618f4e1a0c18480a6ae5bae5
McGaelen <mcinteeg@mail.gvsu.edu>
2025-08-14 22:58:09 -0400

    floating toolbar with stock tldraw UI works!

---
+++ src/HandwritingStateManager.ts
@@ -0,0 +1,35 @@
+import { App, TFile } from 'obsidian'
+import { produce } from 'immer'
+
+const defaultState: HandwritingState = {
+  height: 500,
+  floatingToolbar: {
+    x: 0,
+    y: 0,
+  },
+}
+
+export class HandwritingStateManager {
+  #app: App
+  #file: TFile
+  #state: HandwritingState
+
+  constructor(
+    app: App,
+    file: TFile,
+    initialState: HandwritingState | undefined,
+  ) {
+    this.#app = app
+    this.#file = file
+    this.#state = initialState ?? defaultState
+  }
+
+  get current() {
+    return this.#state
+  }
+
+  update(producer: (draft: HandwritingState) => void) {
+    this.#state = produce(this.#state, producer)
+    this.#app.vault.modify(this.#file, JSON.stringify(this.#state))
+  }
+}
---
+++ src/react/StateManagerContext.tsx
@@ -0,0 +1,16 @@
+import { createContext, PropsWithChildren } from 'react'
+import { HandwritingStateManager } from '../HandwritingStateManager'
+
+// All serializable state goes here
+export const StateManagerContext = createContext({} as HandwritingStateManager)
+
+export function StateManagerProvider({
+  stateManager,
+  children,
+}: PropsWithChildren<{ stateManager: HandwritingStateManager }>) {
+  return (
+    <StateManagerContext.Provider value={stateManager}>
+      {children}
+    </StateManagerContext.Provider>
+  )
+}
