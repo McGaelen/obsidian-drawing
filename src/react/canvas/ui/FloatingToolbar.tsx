b881b55efc82eb786923932063302da5632a9f32
McGaelen <mcinteeg@mail.gvsu.edu>
2025-08-15 20:54:29 -0400

    more floating toolbar improvements

--- src/react/canvas/ui/FloatingToolbar.tsx
+++ src/react/canvas/ui/FloatingToolbar.tsx
@@ -1,10 +1,7 @@
-import { useMarkdownViewRect } from '../../hooks/useMarkdownViewRect'
-import { createPortal } from 'react-dom'
 import { Draggable } from 'gsap/Draggable'
-import { gsap } from 'gsap'
 import { useCallback, useContext, useRef } from 'react'
 import { StateManagerContext } from '../../contexts/StateManagerContext'
-import { DefaultToolbar, DefaultToolbarContent, MobileStylePanel } from 'tldraw'
+import { DefaultToolbar } from 'tldraw'
 
 export function FloatingToolbar(props: {}) {
   const stateManager = useContext(StateManagerContext)
@@ -12,11 +9,11 @@ export function FloatingToolbar(props: {}) {
 
   const handleRef = useCallback((div: HTMLDivElement) => {
     // set initial coordinates
-    gsap.set(div, {
-      x: stateManager.current.floatingToolbar.x,
-      y: stateManager.current.floatingToolbar.y,
-      z: 399,
-    })
+    // gsap.set(div, {
+    //   x: stateManager.current.floatingToolbar.x,
+    //   y: stateManager.current.floatingToolbar.y,
+    //   z: 399,
+    // })
 
     const draggable = new Draggable(div, {
       type: 'x,y',
@@ -43,7 +40,10 @@ export function FloatingToolbar(props: {}) {
     <div
       ref={handleRef}
       className="obsidian-handwriting-toolbar w-fit h-fit overflow-auto absolute bg-(--background-secondary) rounded-[11px] border border-(--divider-color)"
-      style={{ filter: 'drop-shadow(rgba(0, 0, 0, 0.5) 0px 6px 20px)' }}
+      style={{
+        filter: 'drop-shadow(rgba(0, 0, 0, 0.5) 0px 6px 20px)',
+        pointerEvents: 'all',
+      }}
     >
       <DefaultToolbar {...props} />
     </div>
